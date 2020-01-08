0c1f348e305d1d530a766b83d29fb00625d560b5
Trey <treytiller@outlook.com>
2020-01-08 09:30:33 -0500

    Styling to login page midway through

--- src/Routes/LoginPage/LoginPage.js
+++ src/Routes/LoginPage/LoginPage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import LoginForm from "../../Components/Login/Login-Form/Login-Form";
 import { Section } from "../../Components/Utils/Utils";
 import "./LoginPage.css";
+import Logo from "../../../src/Images/Job Hound Logo 260px.png";
 
 export default class LoginPage extends Component {
   static defaultProps = {
@@ -20,7 +21,10 @@ export default class LoginPage extends Component {
   render() {
     return (
       <Section className="LoginPage">
-        <h2>Login</h2>
+        <div className="logo-header">
+          <img src={Logo} alt="job hound logo" className="login-logo" />
+        </div>
+        <h2>Log into Job Hound</h2>
         <LoginForm onLoginSuccess={this.handleLoginSuccess} />
       </Section>
     );
